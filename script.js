d0bbf71ebfa0cda55a2ebec857014de180206122
SimasDei <simondeiman@gmail.com>
2019-02-20 17:56:56 +0200

    arrow functions and global  this

--- script.js
+++ script.js
@@ -80,3 +80,114 @@ const n = `${firstName} ${lastName}`;
 console.log(n.startsWith('J'));
 console.log(n.endsWith('h'));
 console.log(n.includes(' '));
+console.log(`${n} `.repeat(2));
+
+/**
+ * Concept: Arrow Functions
+ */
+
+const years = [1998, 1965, 1972, 1937];
+
+// ES5
+var ages5 = years.map(function(element) {
+  return 2019 - element;
+});
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(element => 2019 - element);
+console.log(ages6);
+
+ages6 = years.map(
+  (element, index) => `Age element: ${index + 1}: ${2019 - element}`
+);
+console.log(ages6);
+
+ages6 = years.map((element, index) => {
+  const now = new Date().getFullYear();
+  const age = now - element;
+  return `Age element ${index + 1}: ${age}`;
+});
+console.log(ages6);
+
+//ES5
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var _this = this;
+    document.querySelector('.green').addEventListener('click', function() {
+      var str =
+        'This is box number ' +
+        _this.position +
+        ' and the color is: ' +
+        _this.color;
+      alert(str);
+    });
+  }
+};
+// box5.clickMe();
+
+//ES6
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str =
+        'This is box number ' +
+        this.position +
+        ' and the color is: ' +
+        this.color;
+      alert(str);
+    });
+  }
+};
+
+// box6.clickMe();
+/*
+const box66 = {
+  color: 'green',
+  position: 1,
+  clickMe: () => {
+    document.querySelector('.green').addEventListener('click', () => {
+      var str =
+        'This is box number ' +
+        this.position +
+        ' and the color is: ' +
+        this.color;
+      alert(str);
+    });
+  }
+};
+box66.clickMe();
+ */
+
+function Person(name) {
+  this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(element) {
+      return this.name + ' is friends with ' + element;
+    }.bind(this)
+  );
+  console.log(arr);
+};
+
+var friends = ['Bobby', 'Jennifer', 'Marky-Mark'];
+
+new Person('Jimbo').myFriends5(friends);
+
+//ES6
+
+Person.prototype.myFriends6 = function(friends) {
+  const arr = friends.map(element => `${this.name} is friends with ${element}`);
+  console.log(arr);
+};
+
+var friends = ['Bobby', 'Jennifer', 'Marky-Mark'];
+
+new Person('Fields').myFriends6(friends);
